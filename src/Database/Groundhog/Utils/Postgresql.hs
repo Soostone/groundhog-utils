1f3a555e2c3475df21e9ce749a60ce08652adf1f
Ozgun Ataman <ozgun.ataman@soostone.com>
2014-07-16 14:03:12 -0400

    Add insertMany for bulk PG inserts

--- src/Database/Groundhog/Utils/Postgresql.hs
+++ src/Database/Groundhog/Utils/Postgresql.hs
@@ -1,18 +1,31 @@
 {-# LANGUAGE FlexibleContexts          #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE OverloadedStrings         #-}
+
 module Database.Groundhog.Utils.Postgresql
-    ( module Database.Groundhog.Utils.Postgresql
+    ( pg
+    , keyToInt
+    , intToKey
+    , keyToIntegral
+    , integralToKey
+    , insertMany
+
     , U.getKey
     , U.mkKey
     , U.SC (..)
     , U.Sh (..)
+
     ) where
 
 -------------------------------------------------------------------------------
+import           Control.Monad
+import qualified Data.ByteString.Char8          as B
 import           Database.Groundhog.Core
+import           Database.Groundhog.Generic
+import           Database.Groundhog.Generic.Sql
 import           Database.Groundhog.Postgresql
 -------------------------------------------------------------------------------
-import qualified Database.Groundhog.Utils      as U
+import qualified Database.Groundhog.Utils       as U
 -------------------------------------------------------------------------------
 
 
@@ -34,5 +47,36 @@ integralToKey = U.integralToKey pg
 
 intToKey :: PrimitivePersistField (Key a b) => Int -> Key a b
 intToKey = U.intToKey pg
+
+
+
+
+
+toEntityPersistValues' v = ($ []) `liftM` toEntityPersistValues v
+
+
+-------------------------------------------------------------------------------
+insertMany
+    :: (PersistEntity a, PersistBackend m, PrimitivePersistField (AutoKey a))
+    => [a]
+    -> m [AutoKey a]
+insertMany vs = do
+    vs' <- (concat . map tail) `liftM` mapM toEntityPersistValues' vs
+    queryRaw False query vs' (mapAllRows converter)
+  where
+    converter [x] = return $ fromPrimitivePersistValue pg x
+
+    query = B.unpack . fromUtf8 $
+      "INSERT INTO " <> dbName <> " (" <> fieldNames <> ")" <>
+      " VALUES " <> placeholders <> " returning(id)"
+
+    placeholders = commasJoin $ map mkPlace vs
+
+    mkPlace _ = "(" <> commasJoin (map (const "?") fields) <> ")"
+
+    entity = entityDef pg (head vs)
+    fieldNames = commasJoin $ map (fromString . fst) fields
+    fields = constrParams $ head (constructors entity)
+    dbName = mainTableName id entity
 
 
